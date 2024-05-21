49adc9b20714db9bd86ab8c71e33adf2723f202c
Anton Lisitsa <lisica-97@mail.ru>
2024-05-21 14:06:16 +0700

    Первый коммит

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,8 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
+    'api',
 ]
 
 MIDDLEWARE = [
