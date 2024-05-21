9c610e64a9d82ebae22bb15b1bca9d72c255515c
Anton Lisitsa <lisica-97@mail.ru>
2024-05-21 14:15:34 +0700

    2 коммит

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'reviews.apps.ReviewsConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
