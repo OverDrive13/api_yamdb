18c7d6b3589f165b7c4b4ff13cedaa7ba23d4fc2
Anton Lisitsa <lisica-97@mail.ru>
2024-05-28 23:16:29 +0700

    fix_import_2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -82,7 +82,7 @@ class ReviewSerializer(serializers.ModelSerializer):
             title_id = self.context['view'].kwargs['title_id']
             if user.reviews.filter(title__id=title_id).exists():
                 raise serializers.ValidationError(
-                    'Пользователь может оставить только один отзыв на произведение'
+                    'Пользователь может оставить только один отзыв'
                 )
         return data
 
