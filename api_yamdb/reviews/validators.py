19e8a7f1a65c9d16b5acc606da8db015336a59ff
Александр Толстиков <sky-er@MacBook-Air-Aleksandr-2.local>
2024-05-28 13:27:02 +0300

    user_serilaizer fix

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -10,8 +10,26 @@ def year_validator(value):
         )
 
 
-def validate_username(self, username):
+def validate_username(username):
     if username == 'me':
         raise ValidationError(
             'Недопустимое имя пользователя!'
         )
+
+
+def validate_email(email):
+    from .models import User
+    if User.objects.filter(email=email).exists():
+        raise ValidationError(
+            'Пользователь с таким email уже зарегистрирован'
+        )
+    return email
+
+
+def validate_username_exists(username):
+    from .models import User
+    if User.objects.filter(username=username).exists():
+        raise ValidationError(
+            'Пользователь с таким именем уже зарегистрирован'
+        )
+    return username
