4809bb154749a695419d6ab935764cb07c60c07a
Александр Толстиков <sky-er@MacBook-Air-Aleksandr-2.local>
2024-05-29 14:22:26 +0300

    Проверка роли юзера вынесена в методы модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -48,7 +48,11 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return self.role == UserRole.ADMIN.value or self.is_superuser
+        return self.role == UserRole.ADMIN or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == UserRole.MODERATOR
 
     def __str__(self):
         return self.username
